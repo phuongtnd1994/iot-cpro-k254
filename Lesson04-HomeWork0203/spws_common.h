da298dc15c5ca8ac664156caf11f23a68fa955cc
phuongtnd <phuongtnd.neu@gmail.com>
2025-09-26 15:17:14 +0700

    Start HomeWork0203

---
+++ Lesson04-HomeWork0203/spws_common.h
@@ -0,0 +1,16 @@
+#ifndef SPWS_COMMON_H
+#define SPWS_COMMON_H
+
+typedef enum
+{
+    MODE_AUTO = 0,
+    MODE_MANUAL = 1
+} SystemMode;
+
+typedef enum
+{
+    PUMP_OFF = 0,
+    PUMP_ON = 1
+} PumpState;
+
+#endif
