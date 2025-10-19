2c4476336139521822d9f72ce723b9758065d00b
phuongtnd <phuongtnd.neu@gmail.com>
2025-10-19 22:41:45 +0700

    Cau 44

---
+++ OnTap01/BaiTap01/44.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+union Frame
+{
+    struct
+    {
+        unsigned short len : 10;
+        unsigned short type : 3;
+        unsigned short crc : 3;
+    } info;
+
+    struct
+    {
+        unsigned char low;
+        unsigned char high;
+    } bytes;
+};
+
+int main(void)
+{
+    // printf("Size of  unsigned short : %ld\n", sizeof(unsigned short));
+
+    printf("Size of union Frame: %ld\n", sizeof(union Frame));
+
+    return 0;
+}
